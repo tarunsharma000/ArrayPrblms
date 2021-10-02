d0b8ef663c7f3ceb3e78724ddc4f484b2c01f28d
TARUN SHARMA <tarun.sharmad35@gmail.com>
2021-10-02 16:18:08 +0530

    FunctionCommit

---
+++ FindFactorialLargeNo.cpp
@@ -0,0 +1,21 @@
+void multiply(int n, vector<int>& number) {
+        int carry = 0;
+        for (int i = 0; i < number.size(); i++) {
+            int num = n * number[i];
+            number[i] = (char)((num + carry) % 10);
+            carry = (num + carry) / 10;
+        }
+        while (carry) {
+            number.push_back(carry % 10);
+            carry /= 10;
+        }
+    }
+    vector<int> factorial(int N){
+        vector<int> number;
+        number.push_back(1);
+        for (int i = 2; i <= N; i++) 
+            multiply(i, number);
+        reverse(number.begin(), number.end()); 
+        return number;
+    }
+//ONLY FUNCTION.
